5568a12bb875cfd15701bd1926f6cbe4d0c3ac7a
Jan Koch <jan@jkoch.me>
2019-09-04 19:38:18 +0200

    🐛 FIX: Fixed autocorrect errors in admin

--- admin/partials/wpmo-admin-display.php
+++ admin/partials/wpmo-admin-display.php
@@ -15,11 +15,11 @@
 wp_nonce_field( 'wpmo-trigger-cancelled-subscription-export', 'wpmo_export_nonce' );
 ?>
 <p>Click the button below to trigger the export of the cancelled and pending-cancellation subscriptions.</p>
-<button id='wpmo-trigger-cancelled-subscription-export' class="button button - primary'>Start export</button>
-<span id='wpmo - running'>Please wait. Export is running.</span>
-<div id='wpmo - success - notice'>
-	<p>Success. <a href=' < ? php echo esc_url( get_site_url() ); ?>/wp-content/uploads/cancelled-subscriptions.csv">Download export file here.</a></p>
-	</div>
-	<div id="wpmo-error-notice">
-		<p></p>
-	</div>
+<button id='wpmo-trigger-cancelled-subscription-export' class="button button-primary">Start export</button>
+<span id='wpmo-running'>Please wait. Export is running.</span>
+<div id='wpmo-success-notice'>
+	<p>Success. <a href="<?php echo esc_url( get_site_url() ); ?>/wp-content/uploads/cancelled-subscriptions.csv">Download export file here.</a></p>
+</div>
+<div id="wpmo-error-notice">
+	<p></p>
+</div>
